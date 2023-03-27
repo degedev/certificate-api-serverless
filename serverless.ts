d2e0e05b797adaf93ddbb5e95d8507df8d217a89
Douglas Ribeiro <dgrb0325@gmail.com>
2023-03-27 14:55:33 -0300

    deploy

--- serverless.ts
+++ serverless.ts
@@ -61,7 +61,7 @@ const serverlessConfiguration: AWS = {
       ],
     }
   },
-  package: { individually: true },
+  package: { individually: false, include: ["./src/templates/**"] },
   custom: {
     esbuild: {
       bundle: true,
